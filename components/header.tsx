ae5a62d86abd75eacd11cf6172662d49d07c3cf1
MacBook Pro <macbookpro@Ziyods-MacBook-Pro.local>
2024-07-22 16:50:00 +0500

    fixed

--- components/header.tsx
+++ components/header.tsx
@@ -3,15 +3,17 @@ import {Logo} from "@/svg"
 
 const Index = () => {
   return (
-    <header className="container py-[34px] flex items-center justify-between">
+    <header className="container py-[34px] flex items-center justify-between sm:p-[20px]">
         <a href="#"><Image src={Logo} alt="logo"/></a>
         <ul className="flex gap-[50px] items-center">
-            <li className="text-[#fff] font-[600] text-[16px] cursor-pointer">Marketplace</li>
-            <li className="text-[#fff] font-[600] text-[16px] cursor-pointer">Rankings</li>
-            <li className="text-[#fff] font-[600] text-[16px] cursor-pointer">Connect a wallet</li>
-            <li className="ml-[-20px]">
+            <li className="text-[#fff] sm:hidden font-[600] text-[16px] cursor-pointer">Marketplace</li>
+            <li className="text-[#fff] sm:hidden font-[600] text-[16px] cursor-pointer">Rankings</li>
+            <li className="text-[#fff] sm:hidden font-[600] text-[16px] cursor-pointer">Connect a wallet</li>
+            <li className="ml-[-20px] sm:hidden">
                 <button className="flex items-center gap-[12px] text-[#fff] font-[600] text-[16px] py-[19px] hover:bg-[#a159ffe1] active:bg-[#A259FF] transition bg-[#A259FF] pl-[34px] pr-[30px] rounded-[20px]"><i className='bx bx-user text-[20px] font-[1000px]'></i> Sign Up</button>
             </li>
+
+            <li className="cursor-pointer hidden text-[34px] sm:flex text-[#fff]"><i className='bx bx-menu'></i></li>
         </ul>
     </header>
   )
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
 import type { Config } from "tailwindcss";
 
+
 const config: Config = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
@@ -14,6 +15,23 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+
+      screens: {
+      '2xl': {'max': '1535px'},
+      // => @media (max-width: 1535px) { ... }
+
+      'xl': {'max': '1279px'},
+      // => @media (max-width: 1279px) { ... }
+
+      'lg': {'max': '1023px'},
+      // => @media (max-width: 1023px) { ... }
+
+      'md': {'max': '767px'},
+      // => @media (max-width: 767px) { ... }
+
+      'sm': {'max': '639px'},
+      // => @media (max-width: 639px) { ... }
+    }
   },
   plugins: [],
 };
